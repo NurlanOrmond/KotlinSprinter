7a68fb86a64f3bb0ab4666bdea9b8a9a8deb1928
Nurlan Ormond <sunshineforester@gmail.com>
2023-03-21 22:20:45 +0300

    KS-1-1 Task 01 is done

--- src/main/kotlin/Lesson01/Lesson01_Task01.kt
+++ src/main/kotlin/Lesson01/Lesson01_Task01.kt
@@ -1,6 +1,10 @@
 package Lesson01
 
 fun main() {
-    println("Task 1 of Lesson 1")
+    var quantityOfOrders: Int = 75
+    val positiveFeedBack001: String = "I appreciate your high level service and wish you prosperity!"
+
+    println(quantityOfOrders)
+    println(positiveFeedBack001)
 
 }
